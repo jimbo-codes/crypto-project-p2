11181362e4b38a737c20f17ba22fbbf9d0a98766
James Burns <jrburns96@aol.com>
2021-12-09 18:18:10 -0500

    links one more time

--- src/components/CryptoPage.js
+++ src/components/CryptoPage.js
@@ -3,6 +3,7 @@ import SearchBar from "./SearchBar";
 import ProjectDetail from "./ProjectDetail";
 import CryptoList from './CryptoList';
 import { Route, Routes } from "react-router";
+import TopMovers from "./TopMovers";
 
 function CryptoPage({name, table, global }) {
     const [detail, setDetail] = useState([]);
@@ -13,6 +14,7 @@ function CryptoPage({name, table, global }) {
             <Routes>
                 <Route path=":id" element={<ProjectDetail detail={detail} setDetail={setDetail}/>} />
             </Routes>
+            {/* <TopMovers/> */}
             <CryptoList table={table}/>
         </div>
       );
