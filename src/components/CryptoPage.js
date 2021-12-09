396689fda3b3d23a009422178738c380602f6267
Prajay Tipre <prajaytipre@gmail.com>
2021-12-09 12:42:24 -0500

    Working Project Detail

--- src/components/CryptoPage.js
+++ src/components/CryptoPage.js
@@ -2,7 +2,6 @@ import React,{useState} from "react";
 import SearchBar from "./SearchBar";
 import ProjectDetail from "./ProjectDetail";
 import CryptoList from './CryptoList';
-import SelectedData from "./SelectedData";
 import { Route, Routes } from "react-router";
 
 // This is our top level actual application component. (A child of User?) it holds:
@@ -18,17 +17,12 @@ function CryptoPage({name, table}) {
 
     // The full coin data gets passed down to render -> List, List does the Data & repeat
     const [selected, setSelected] = useState(false)
-    const [detail, setDetail] = useState({});
+    const [detail, setDetail] = useState([]);
 
 
     return (      
         <div>
             <SearchBar/>
-            <div className="py-4">
-                {selected?<SelectedData/>:null}
-                {/* Set this "selected" state on search or button press. */}
-                Hi {name}!! This is the Main parent component for our actual application
-            </div>
             <Routes>
                 <Route path=":id" element={<ProjectDetail detail={detail} setDetail={setDetail}/>} />
             </Routes>
